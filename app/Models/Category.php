547e0b456c34a3846a1aa75b8d7b3fa2a779eae4
leandro.villalobos.dx@gmail.com <Lea23VC>
2022-10-14 01:26:50 -0300

    :construction: Create Product controller
    Add table name in model and start resource code

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,17 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use EloquentFilter\Filterable;
 
 class Category extends Model
 {
     use HasFactory, Filterable;
 
     protected $fillable = [
-        'message',
-        'user_id',
-        'marker_id',
-        'likes',
-        'dislikes',
-        'id'
+        'name',
     ];
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use EloquentFilter\Filterable;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, Filterable;
+
+    protected $fillable = [
+        'name',
+        'url_image',
+        'price',
+        'discount',
+        'category',
+
+    ];
+
+    protected $table = 'product';
 }
